c0577c3f8f91ca6101d3cc8da19adc83f3175ba0
LoKi <sand5088@gmail.com>
2022-03-19 15:52:11 +0700

    add js and css with custom-font

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
 import "bootstrap"
+import "custom/purecounter"
+import "custom/isotope.pkgd"
+import "custom/swiper-bundle.min"
+import "custom/noframework.waypoints"
+
 import "custom/main"
